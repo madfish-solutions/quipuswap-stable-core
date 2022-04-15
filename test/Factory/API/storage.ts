7944353ae14e19fd28f3589a20369d85828cf02a
Illia Likhoshva <ilyaliko64@gmail.com>
2022-04-15 15:22:58 +0300

    Fix storage typings

--- test/Factory/API/storage.ts
+++ test/Factory/API/storage.ts
@@ -14,6 +14,7 @@ export const factory_storage: FactoryStorage = {
     init_price: new BigNumber(0),
     burn_rate_f: new BigNumber(0),
     pools_count: new BigNumber(0),
+    pool_id_to_address: new MichelsonMap(),
     pool_to_address: new MichelsonMap(),
     quipu_token: null as FA2,
     quipu_rewards: new BigNumber(0),
